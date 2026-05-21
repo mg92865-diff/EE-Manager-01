8d285f8f2a54224611df4bb64ace8a289247b9bb
mg92865-diff <mg92865@gmail.com>
2026-05-21 16:21:11 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-﻿const CACHE_NAME = 'records-cache-v1';
+﻿const CACHE_NAME = 'records-cache-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://unpkg.com/dexie@3.2.4/dist/dexie.js'
 ];
 
